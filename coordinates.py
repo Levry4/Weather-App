d6bb5b238bc37783339a5480ee00bd69141be8a2
LevRyb <leffrybalko@mail.ru>
2025-11-24 20:58:51 +0300

    Были перепутаны модули coordinates и weather, проблема решена, код изменен

--- coordinates.py
+++ coordinates.py
@@ -1,12 +1,1 @@
-from coordinates import get_coordinates
-from weather_api_service import get_weather
-from weather_formatter import format_weather
 
-def main():
-    coordinates = get_coordinates()
-    weather_data = get_weather(coordinates)
-    output = format_weather(weather_data)
-    print(output)
-
-if __name__ == "__main__":
-    main()
