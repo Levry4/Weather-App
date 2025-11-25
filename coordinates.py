baaf33fcbdb3fddb13ef1d27e91fc9846a3d29be
LevRyb <leffrybalko@mail.ru>
2025-11-25 16:12:15 +0300

    Исправлена проблема с глобальным доступом API ключа, создан новый

--- coordinates.py
+++ coordinates.py
@@ -1,10 +1,15 @@
+import os
 import requests
+
 def get_coordinates(city_name):
-    api_key = "dcec2117d017a73ca21c319fdf80b7a3"  
+    api_key = os.getenv("OPENWEATHER_API_KEY")
+    if not api_key:
+        raise RuntimeError("API ключ не найден! Установите переменную окружения OPENWEATHER_API_KEY")
+
     url = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit=1&appid={api_key}"
 
     response = requests.get(url)
-    response.raise_for_status()  
+    response.raise_for_status()
 
     data = response.json()
 
@@ -15,5 +20,6 @@ def get_coordinates(city_name):
     longitude = data[0]['lon']
 
     return latitude, longitude
+
 
 
